36daa1c38e6ba17db37f923bb72b9e627762d6a9
Nathan Appere <nathan.appere@gmail.com>
2021-08-15 18:31:23 +0200

    Add avatar

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
     %w(primary secondary success danger warning info light dark)
   end
 
+  def breakpoints
+    %w(xs sm md lg xl xxl)
+  end
+
+
+  def image_people
+    @image_people ||= %w(
+      1-pexels-brett-sayles-3118694.jpg
+      2-pexels-vazhnik-7562076.jpg
+      3-pexels-yuri-manei-2690323.jpg
+      4-pexels-hudson-marques-3328072.jpg
+      5-pexels-rodrigo-souza-2658834.jpg
+      6-pexels-aidil-bahaman-2748242.jpg
+      7-pexels-moises-patricio-9159678.jpg
+      8-pexels-mwabonje-1812634.jpg
+    ).map do |filename|
+      "img/people/#{ filename }"
+    end
+  end
+
 end
