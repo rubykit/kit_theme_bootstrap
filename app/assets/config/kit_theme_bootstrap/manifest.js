642599f544961625bdcc27d1ac6a97dd8fd874e5
Nathan Appere <nathan.appere@gmail.com>
2021-08-26 17:16:51 +0200

    Rename theme manifest

---
+++ app/assets/config/kit_theme_bootstrap/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../../vendor
+//= link_tree ../../stylesheets
+
+//= link_directory ../../javascripts/kit_theme_bootstrap .js
+//= link_directory ../../stylesheets/kit_theme_bootstrap .css
+
+//= link_directory ../../vendor/kit_theme_bootstrap .js
+//= link_directory ../../vendor/kit_theme_bootstrap .css
