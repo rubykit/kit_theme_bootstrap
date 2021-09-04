4010c784121e87e130faf51bbba5d67a5c1d90e0
Nathan Appere <nathan.appere@gmail.com>
2021-09-04 19:16:11 +0200

    General cleanup

--- app/assets/vendor/kit_theme_bootstrap/vendor.js
+++ app/assets/vendor/kit_theme_bootstrap/vendor.js
@@ -4,10 +4,9 @@
 
 //= require rails-ujs
 
-//= require ./vendor/bs4-toast/toast.1_2_0
-//= require ./vendor/clipboard/clipboard.2_0_6
-//= require ./vendor/anchorjs/anchor.4_3_1
-
+//= require ./vendor/bs4-toast/1.2.0/toast
+//= require ./vendor/clipboard/2.0.6/clipboard
+//= require ./vendor/anchorjs/4.3.1/anchor
 //= require ./vendor/jquery_mask/1.14.16/jquery.mask
 
 //= require ./vendor/select2/4.1.0-rc.0/js/select2
