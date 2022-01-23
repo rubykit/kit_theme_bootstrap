aa0b24631246b53eddecfa4c49d9d09403391d52
Nathan Appere <nathan.appere@gmail.com>
2022-01-23 11:22:42 +0100

    Add Toastr dependency to replace bs4-toast

--- app/assets/vendor/kit_theme_bootstrap/vendor.js
+++ app/assets/vendor/kit_theme_bootstrap/vendor.js
@@ -4,12 +4,17 @@
 
 //= require rails-ujs
 
-//= require ./vendor/bs4-toast/1.2.0/toast
+// require ./vendor/bs4-toast/1.2.0/toast
+//= require ./vendor/toastr/2.1.4/toastr
 //= require ./vendor/clipboard/2.0.6/clipboard
 //= require ./vendor/anchorjs/4.3.1/anchor
 //= require ./vendor/jquery_mask/1.14.16/jquery.mask
+
+//= require_self
 
 //= require ./vendor/select2/4.1.0-rc.0/js/select2
 //= require ./vendor/select2/4.1.0-rc.0/js/i18n/en
 
 //= require ./vendor/bootstrap-toc/1.0.1/bootstrap-toc
+
+console.log("LOADED")
